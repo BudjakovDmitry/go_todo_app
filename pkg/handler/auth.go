3869595cecdf19d6e14e70480dc18e0a3752f988
Dmitry Budyakov <dmitry.budyakov@gmail.com>
2026-01-14 12:22:55 +0300

    User sign-up

--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -1,10 +1,27 @@
 package handler
 
 import (
+	"net/http"
+
+	"github.com/BudjakovDmitry/go_todo_app"
 	"github.com/gin-gonic/gin"
 )
 
 func (h *Handler) signUp(c *gin.Context) {
+	var input todo.User
+
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	id, err := h.services.CreateUser(input)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{"id": id})
 
 }
 
